13940aece577e9bd4e8226285ab65d6f2c1b9a66
sinamashini <sina.mashini1993@gmail.com>
2025-02-06 22:39:10 +0330

    feat: adding the docker

--- obj/Debug/net9.0/MyWebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5fa79a8b8509513e2077ea2f08a080862278f2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
